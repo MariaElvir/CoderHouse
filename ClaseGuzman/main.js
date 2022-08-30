e4cb98a68681582f53127ad7a51af36893aaeb64
MariaElvir <112521512+MariaElvir@users.noreply.github.com>
2022-08-30 18:09:04 -0500

    Add files via upload

---
+++ ClaseGuzman/main.js
@@ -0,0 +1,12 @@
+let ingreseNumero=prompt('Ingrese el número de veces que quiere que se repita la palabra "HOLA": ')
+if(ingreseNumero !=""){
+        for (let i = 0; i < ingreseNumero; i++) {
+                alert('HOLA');                
+        }
+} else{
+        alert('No a ingresado ningún número');
+}
+
+
+
+
